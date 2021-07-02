0d27bd4ed39406084e55c5dc54de919c8aa04e41
rizkidwimartanto <rizkidwimartanto@gmail.com>
2021-07-03 02:04:00 +0700

    mengubah lagi

--- RSA_Kripto.m
+++ RSA_Kripto.m
@@ -1,14 +1,14 @@
-clc;%Clear command window
+clc;% Untuk clear command window
 disp('RSA Encrypt-Decrypt Algorithm');
 disp('-----------------------------------------');
-clear all; close all;%Clear variables in workspace and close figure
+clear all; close all;% Untuk clear variabel
 
-%Input value of p and q
+% Menginput nilai p dan q
 p = input('\nMasukkan Nilai p: ');
 q = input('\nMasukkan Nilai q: ');
 
 disp('Intaializing:');
-n=p*q;%Calculate value of n
+n=p*q;% Perkalian dari p * q
 tf=(p-1)*(q-1);%Calculate value of totien function
 
 %Calculate the value of e
@@ -26,14 +26,14 @@ while i > 0
     x = e*d;
     x = mod(x,20);
     if x == 1
-       i = 0; 
+        i = 0; 
     end
 end
 
 %clc;
 disp(['Nilai (p) adalah: ' num2str(p)]);
 disp(['Nilai (q) adalah: ' num2str(q)]);
-disp(['Nilai (N) adalah: ' num2str(n)]);
+disp(['Nilai (n) adalah: ' num2str(n)]);
 disp(['Nilai totien function (tf) adalah: ' num2str(tf)]);
 disp(['Public key (e) adalah: ' num2str(e)]);
 disp(['Private key (d) adalah: ' num2str(d)]);
