6bd75e4b10f7f62667d082c4b861ce6b5cf75b4c
denrahil <56780326+dendii2601@users.noreply.github.com>
2021-07-03 12:00:48 +0700

    ganti

--- RSA_Kripto.m
+++ RSA_Kripto.m
@@ -9,24 +9,24 @@ q = input('\nMasukkan Nilai q: ');
 
 disp('Intaializing:');
 n=p*q;% Perkalian dari p * q
-tf=(p-1)*(q-1);%Calculate value of totien function
+tf=(p-1)*(q-1);%Menghitung nilai fungsi totien
 
 %Calculate the value of e
-x=2;e=1;
+x=2;y=1;
 while x > 1
-    e=e+1;
-    x=gcd(tf,e);%Search greatest common division
+    y=y+1;
+    x=gcd(tf,y);%Cari divisi umum terbesar
 end
 
 %Calculate the value of d
 i=1;
-d=0;
+a=0;
 while i > 0
-    d = d+1;
-    x = e*d;
+    a = a+1;
+    x = y*a;
     x = mod(x,20);
     if x == 1
-       i = 0; 
+        i = 0; 
     end
 end
 
@@ -35,8 +35,8 @@ disp(['Nilai (p) adalah: ' num2str(p)]);
 disp(['Nilai (q) adalah: ' num2str(q)]);
 disp(['Nilai (n) adalah: ' num2str(n)]);
 disp(['Nilai totien function (tf) adalah: ' num2str(tf)]);
-disp(['Public key (e) adalah: ' num2str(e)]);
-disp(['Private key (d) adalah: ' num2str(d)]);
+disp(['Public key (e) adalah: ' num2str(y)]);
+disp(['Private key (d) adalah: ' num2str(a)]);
 
 P = input('\nMasukkan pesan yang akan dikirim: ','s');
 c = double(P);
@@ -48,18 +48,15 @@ disp(c);
 
 %Encrypt
 
-cipher= power(c,e);
+cipher= power(c,y);
 cipher= mod(cipher,n);
 
 disp('cipher: ');
 disp(cipher);
 
 %Decrypt
-plain= power(cipher,d);
+plain= power(cipher,a);
 plain= mod(plain,n)+33;
 
 disp('decrypt: ');
 disp(plain);
-
-disp('Pesan hasil decrypt: ');
-disp(char(plain));%Convert double to char
